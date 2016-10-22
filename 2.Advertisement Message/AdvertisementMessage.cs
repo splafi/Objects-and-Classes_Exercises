5244578ceb0bbe0479c0c9b28d90b6c7f9476a5a
splafi <martin_petkov555@abv.bg>
2016-10-22 17:13:04 +0300

    Unfinished

---
+++ 2.Advertisement Message/AdvertisementMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Objects_and_Classes
+{
+
+    class exercises
+    {
+        static void Main(string[] args)
+        {
+            int messagesNumber = int.Parse(Console.ReadLine());
+            string[] phrases = new string[] {
+                "Excellent product.","Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."
+            };
+            string[] events = new string[] {
+                "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"
+            };
+            string[] authors = new string[] {
+                "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"
+            };
+            string[] towns = new string[] {
+                "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"
+            };
+            Random number = new Random();
+            for (int message = 0; message < messagesNumber; message++)
+            {
+                int phraseIndex = number.Next(0, phrases.Length);
+                int eventIndex = number.Next(0, events.Length);
+                int authorIndex = number.Next(0, authors.Length);
+                int townIndex = number.Next(0, towns.Length);
+
+                Console.WriteLine(phrases[phraseIndex] + " " + events[eventIndex] + " " + authors[authorIndex] + " - " + towns[townIndex]);
+            }
+        }
+    }
+}
---
+++ 3. Intersection of Circles/IntersectionOfCircles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Intersection_of_Circles
+{
+    class IntersectionOfCircles
+    {
+        static void Main(string[] args)
+        {
+            CircleInput c1 = new CircleInput();
+            CircleInput c2 = new CircleInput();
+           bool trueFalse = Intersect(c1,c2);
+        }
+        static bool Intersect(CircleInput[] c1, CircleInput[] c2)
+        {
+            if(CalcDistance>(c1[2]+c2[2]))
+            {
+
+            }
+            return trueFalse:
+        }
+
+        static double CalcDistance(CircleInput p1, CircleInput p2)
+        {
+            int deltaX = p2.X - p1.X;
+            int deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+        static CircleInput ReadPoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            CircleInput circle = new CircleInput();
+            circle.X = pointInfo[0];
+            circle.Y = pointInfo[1];
+            circle.R = pointInfo[2];
+            return circle;
+        }
+    }
+    class CircleInput
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int R { get; set; }
+    }
+}
